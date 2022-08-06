a562374f62e077ead9482820762527a5d7d7e29d
Ashwani Kumar <107989407+The-Ashwani-Kumar@users.noreply.github.com>
2022-08-06 23:20:48 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,14 @@ export default function Navbar(props) {
         <>
             <nav className={`navbar navbar-expand-lg navbar-${props.toggleMode()} bg-${props.mode}`}>
                 <div className="container-fluid" style={{backgroundColor:'violet', border:'2px solid white', borderRadius:20, margin:5}}>
-                    <a className="navbar-brand" href="/">{props.title}</a>
+                    <a className="navbar-brand" href="#">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/about">{props.myText}</a>
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                         </ul>
                         {/* <div className={`form-check form-switch`}>
