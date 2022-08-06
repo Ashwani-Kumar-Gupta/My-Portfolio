59082fd76baf2279dc948ed82501f1f7d52d97ba
Ashwani Kumar <107989407+The-Ashwani-Kumar@users.noreply.github.com>
2022-08-06 23:41:35 +0530

    Add files via upload

--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -40,7 +40,7 @@ export default function TextFrom(props) {
         <div style={{color : props.mode==='dark' ? 'white' : 'black'}}>
             <h2 className='my-3'>{props.heading}</h2>
             <div className="mb-3">
-                <textarea style={{backgroundColor : props.mode==='dark' ? 'black' : 'white', color : props.mode==='light' ? 'black' : 'white'}} rows="8" className="form-control my-3" value={text} onChange={handleOnChange} id="myBox"/>
+                <textarea style={{backgroundColor : props.mode==='dark' ? 'black' : 'white', color : props.mode==='light' ? 'black' : 'white'}} rows="8" className={`form-control my-3 bg-${props.mode}`} value={text} onChange={handleOnChange} id="myBox"/>
             </div>
             <button className={`btn btn-primary mx-2 bg-${props.mode==='primary'?'black':'primary'}`} onClick={upperCase}>Convert to Uppercase</button>
             <button className={`btn btn-primary mx-2 bg-${props.mode==='primary'?'black':'primary'}`} onClick={lowerCase}>Convert to Lowercase</button>
